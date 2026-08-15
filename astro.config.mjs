d87c43ea01dc0cf097d6dfc835be6a3426885103
OishiMugicha <yutaro.st1192@gmail.com>
2026-08-16 01:07:30 +0900

    探索アルゴリズムに関する記事を投稿

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { unified } from '@astrojs/markdown-remark';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://oishimugicha.github.io',
   base: '/mugicha-blog',
+  markdown: {
+    processor: unified({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
+  },
 });
